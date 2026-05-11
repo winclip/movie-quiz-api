eb2f3954a2be0c33a829692a5b412494a2d90fca
Sergei Okhotnikov <sokhotnikoff@gmail.com>
2026-05-11 11:27:34 +0200

    initial commit

---
+++ src/main/java/dev/winclip/movie_quiz/MovieQuizApplication.java
@@ -0,0 +1,13 @@
+package dev.winclip.movie_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/winclip/movie_quiz/MovieQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.winclip.movie_quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
